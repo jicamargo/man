1f618e82f46531452e6b7f6f15dbd98d13a3f1fa
jic <ji_camargo@yahoo.com>
2024-07-05 17:49:08 -0500

    add persistence with local storage

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,3 +1,4 @@
+import { songs } from '@/data/data'
 import { Pause, Play } from './Player'
 import { usePlayerStore } from '@/store/playerStore'
 
@@ -23,16 +24,11 @@ export function CardPlayButton ( { id, size = 'small' } ) {
         const { songs, playlist } = data
         const song = songs[0]
         setIsPlaying(true)
-        
         setCurrentMusic( { playlist, songs, song })
-        
-        // actualiza el color del elemento con id="playlist-container"
-        document.getElementById('playlist-container').style.backgroundColor = playlist.color.accent
       })
   }
 
   const iconSize = size === 'small' ? 'w-4 h-4' : 'w-6 h-6'
-
   return (
     <button onClick={handleClick} className={`bg-green-500 rounded-full p-2  hover:border hover:border-green-400 hover:bg-green-400`} >
       { isPlayingPlaylist ? <Pause className={iconSize}/> : <Play className={iconSize}/> }
--- src/store/playerStore.js
+++ src/store/playerStore.js
@@ -1,14 +1,26 @@
 import  { create } from "zustand";
+import { persist } from 'zustand/middleware';
 
-export const usePlayerStore = create((set) => ({
-  isPlaying: false,
-  currentMusic: { playlist: null, song: null, songs: [] },
-  volume: 0.5,
-  previousVolume: 0.5,
-  color: { accent: "#21c872", dark: "#14532d" },
-  setIsPlaying: (isPlaying) => set({ isPlaying }),
-  setCurrentMusic: (currentMusic) => set({ currentMusic }),
-  setVolume: (volume) => set({ volume }),
-  setPreviousVolume: (previousVolume) => set({ previousVolume }),
-  setColor: (color) => set({ color }),
-}));
+export const usePlayerStore = create(persist(
+  (set) => ({
+    isPlaying: false,
+    currentMusic: { playlist: null, song: null, songs: [] },
+    volume: 0.5,
+    previousVolume: 0.5,
+    color: { accent: "#21c872", dark: "#14532d" },
+    setIsPlaying: (isPlaying) => set({ isPlaying }),
+    setCurrentMusic: (currentMusic) => set({ currentMusic }),
+    setVolume: (volume) => set({ volume }),
+    setPreviousVolume: (previousVolume) => set({ previousVolume }),
+    setColor: (color) => set({ color }),
+  }),
+  {
+    name: 'man-player-storage',
+    partialize: (state) => ({
+      currentMusic: state.currentMusic,
+      volume: state.volume
+    }),
+    onRehydrateStorage: () => (state) => {
+      state?.resetIsPlaying();
+    }
+  }));
